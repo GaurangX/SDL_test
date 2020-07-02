17551d88d43cfad871325ab59c490cd46920e5c2
Gaurang Upasani <gaurangupasani@gmail.com>
2020-07-01 17:59:58 -0700

    Added math to enable gradual changes in pixel colors. A better approach would be to map RGB to Hue and Satuartion (next time ;-)).

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 //============================================================================
 
 #include <iostream>
+#include <math.h>
 #include "Screen.h"
 
 using namespace std;
@@ -24,18 +25,22 @@ int main() {
 	while (true) {
 		// update particles
 
-
-
 		// draw particles
+		int elapsed = SDL_GetTicks(); // returns ms.
+		// to limit it to 255
+		unsigned char r = (unsigned char) ((1 + sin(elapsed * 0.0001)) * 128);
+		unsigned char g = (unsigned char) ((1 + sin(elapsed * 0.0002)) * 128);
+		unsigned char b = (unsigned char) ((1 + sin(elapsed * 0.0003)) * 128);
+
 		for (int y = 0; y < Screen::SCREEN_HEIGHT; y++) {
 			for (int x = 0; x < Screen::SCREEN_WIDTH; x++) {
 				// setPixel (x, y, R, G, B)
-				screen.setPixel (x, y, 128, 0, 255, 255);
+				screen.setPixel (x, y, r, g, b, 255);
 			}
 		}
 
 		// one pixel data
-		screen.setPixel(400, 300, 255, 255, 255, 255);
+		// screen.setPixel(400, 300, 255, 255, 255, 255);
 
 		//set update screen with pixel data
 		screen.update();
